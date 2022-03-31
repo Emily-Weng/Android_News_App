921eb216bd1c2f202bd4ca2be49fe16846e456c4
Emily <108703042@nccu.edu.tw>
2022-03-31 19:25:06 +0800

    rr

---
+++ app/src/main/java/com/haerul/popularnews/MainActivity.java
@@ -0,0 +1,411 @@
+package com.haerul.popularnews;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.haerul.popularnews.api.ApiClient;
+import com.haerul.popularnews.api.ApiInterface;
+import com.haerul.popularnews.models.Article;
+import com.haerul.popularnews.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.internal.Util;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements  SwipeRefreshLayout.OnRefreshListener, View.OnClickListener {
+
+    public static final String API_KEY = "ebf410a4a64345f6b0a79e3cde1554a6";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
+    private TextView topHeadline;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout errorLayout;
+    private ImageView errorImage;
+    private TextView errorTitle, errorMessage;
+    private Button btnRetry;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
+    private String curCountry, curCategory;
+    private Button cat1, cat2, cat3, cat4, cat5, cat6, cat7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        topHeadline = findViewById(R.id.topheadelines);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        onLoadingSwipeRefresh("");
+
+        errorLayout = findViewById(R.id.errorLayout);
+        errorImage = findViewById(R.id.errorImage);
+        errorTitle = findViewById(R.id.errorTitle);
+        errorMessage = findViewById(R.id.errorMessage);
+        btnRetry = findViewById(R.id.btnRetry);
+        drawerLayout = findViewById(R.id.navi_drawerlayout);
+        navigationView = findViewById(R.id.drawerview);
+        cat1 = findViewById(R.id.cat1);
+        cat2 = findViewById(R.id.cat2);
+        cat3 = findViewById(R.id.cat3);
+        cat4 = findViewById(R.id.cat4);
+        cat5 = findViewById(R.id.cat5);
+        cat6 = findViewById(R.id.cat6);
+        cat7 = findViewById(R.id.cat7);
+
+        navigationView.setNavigationItemSelectedListener(naviOnItemSelected); // add navigationview items selected listener
+        Drawer_toggle();
+
+        SetListenerOfBtn();
+
+        curCountry = "tw";
+        curCategory = "general";
+
+
+    }
+
+    private void Drawer_toggle(){
+        ActionBar actBar = getSupportActionBar();
+        actBar.setDisplayHomeAsUpEnabled(true);
+        actBar.setHomeButtonEnabled(true);
+
+        //Toolbar toolbar1 = findViewById(R.id.notoolbar);
+        //setSupportActionBar(toolbar1);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+    }
+
+    private void SetListenerOfBtn(){
+        cat1.setOnClickListener(this);
+        cat2.setOnClickListener(this);
+        cat3.setOnClickListener(this);
+        cat4.setOnClickListener(this);
+        cat5.setOnClickListener(this);
+        cat6.setOnClickListener(this);
+        cat7.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.cat1:
+                curCategory="general";
+                break;
+            case R.id.cat2:
+                curCategory="business";
+                break;
+            case R.id.cat3:
+                curCategory="entertainment";
+                break;
+            case R.id.cat4:
+                curCategory="health";
+                break;
+            case R.id.cat5:
+                curCategory="science";
+                break;
+            case R.id.cat6:
+                curCategory="sport";
+                break;
+            case R.id.cat7:
+                curCategory="technology";
+                break;
+
+
+        }
+        LoadJson("", curCountry);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void LoadJson(final String keyword,String country){
+
+        errorLayout.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        //String country = Utils.getCountry();
+        String language = Utils.getLanguage();
+
+        //選擇要抓什麼樣資料的地方
+        Call<News> call;
+
+        if (keyword.length() > 0 ){
+            call = apiInterface.getNewsSearch(keyword, language, "publishedAt", API_KEY, country);
+        } else {
+            call = apiInterface.getNews(country, API_KEY, curCategory);
+        }
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null){
+
+                    if (!articles.isEmpty()){
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                    initListener();
+
+                    topHeadline.setVisibility(View.VISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+
+
+                } else {
+
+                    topHeadline.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+
+                    String errorCode;
+                    switch (response.code()) {
+                        case 404:
+                            errorCode = "404 not found";
+                            break;
+                        case 500:
+                            errorCode = "500 server broken";
+                            break;
+                        default:
+                            errorCode = "unknown error";
+                            break;
+                    }
+
+                    showErrorMessage(
+                            R.drawable.no_result,
+                            "No Result",
+                            "Please Try Again!\n"+
+                            errorCode);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                topHeadline.setVisibility(View.INVISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
+                showErrorMessage(
+                        R.drawable.oops,
+                        "Oops..",
+                        "Network failure, Please Try Again\n"+
+                                t.toString());
+            }
+        });
+
+    }
+
+
+
+    private void initListener(){
+
+        adapter.setOnItemClickListener(new Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                ImageView imageView = view.findViewById(R.id.img);
+                Intent intent = new Intent(MainActivity.this, NewsDetailActivity.class);
+
+                Article article = articles.get(position);
+                intent.putExtra("url", article.getUrl());
+                intent.putExtra("title", article.getTitle());
+                intent.putExtra("img",  article.getUrlToImage());
+                intent.putExtra("date",  article.getPublishedAt());
+                intent.putExtra("source",  article.getSource().getName());
+                intent.putExtra("author",  article.getAuthor());
+
+                Pair<View, String> pair = Pair.create((View)imageView, ViewCompat.getTransitionName(imageView));
+                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        MainActivity.this,
+                        pair
+                );
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    startActivity(intent, optionsCompat.toBundle());
+                }else {
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener naviOnItemSelected =
+            new NavigationView.OnNavigationItemSelectedListener() { //deal with navigationview event
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.country1:
+                            // ...
+                            LoadJson("", "tw");
+                            drawerLayout.closeDrawers();
+                            navigationView.getMenu().getItem(0).setChecked(true);
+                            curCountry = "tw";
+                            break;
+                        case R.id.country2:
+                            // ...
+                            LoadJson("", "us");
+                            drawerLayout.closeDrawers();
+                            navigationView.getMenu().getItem(1).setChecked(true);
+                            curCountry = "us";
+                            break;
+                        case R.id.country3:
+                            // ...
+                            LoadJson("", "gb");
+                            drawerLayout.closeDrawers();
+                            navigationView.getMenu().getItem(2).setChecked(true);
+                            curCountry = "gb";
+                            break;
+                        case R.id.country4:
+                            // ...
+                            LoadJson("", "jp");
+                            drawerLayout.closeDrawers();
+                            navigationView.getMenu().getItem(3).setChecked(true);
+                            curCountry = "jp";
+                            break;
+                        case R.id.country5:
+                            // ...
+                            LoadJson("", "kr");
+                            drawerLayout.closeDrawers();
+                            navigationView.getMenu().getItem(4).setChecked(true);
+                            curCountry = "kr";
+                            break;
+                    }
+                    return false;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Search Latest News...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.length() > 2){
+                    onLoadingSwipeRefresh(query);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Type more than two letters!", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchMenuItem.getIcon().setVisible(false, false);
+
+        return true;
+    }
+
+    @Override
+    public void onRefresh(){
+        LoadJson("", curCountry);
+
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword){
+
+        swipeRefreshLayout.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        LoadJson(keyword, curCountry);
+                    }
+                }
+        );
+
+    }
+
+    private void showErrorMessage(int imageView, String title, String message){
+
+        if (errorLayout.getVisibility() == View.GONE) {
+            errorLayout.setVisibility(View.VISIBLE);
+        }
+
+        errorImage.setImageResource(imageView);
+        errorTitle.setText(title);
+        errorMessage.setText(message);
+
+        btnRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoadingSwipeRefresh("");
+            }
+        });
+
+    }
+
+
+}
